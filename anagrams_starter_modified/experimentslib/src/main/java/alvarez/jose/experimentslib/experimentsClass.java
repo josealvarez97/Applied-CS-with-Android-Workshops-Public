cf990811c86f28b311d7409415bb432270484697
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-02-11 10:09:18 -0800

    Added module for experiments
    Here I'll ode that attendees will be able to play with for getting ideas.

---
+++ anagrams_starter_modified/experimentslib/src/main/java/alvarez/jose/experimentslib/experimentsClass.java
@@ -0,0 +1,10 @@
+package alvarez.jose.experimentslib;
+
+public class experimentsClass {
+
+    public static void main(String[] args) {
+
+        // First things first...
+        System.out.println("HELLO WORLD!!!");
+    }
+}
